24e378da92618a593bc82b14dde48d141965cb1d
paperdeer <tjrgus0713@gmail.com>
2021-09-11 23:42:02 +0900

    [추가] mypage 추가

--- src/App.js
+++ src/App.js
@@ -15,6 +15,128 @@ function App() {
   }
   const [loginStatus,setLoginStatus] = useState(false);
   const [loginModalStatus,setLoginModalStatus] = useState(false);
+  const RecomendGoddsArr = [
+    [
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매1",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매2",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매3",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매4",
+        price : '10000',
+        img : '',
+        location : '여기'
+      }
+    ],
+    [
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매5",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매6",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매7",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매8",
+        price : '10000',
+        img : '',
+        location : '여기'
+      }
+    ],
+    [
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매9",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매10",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매11",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매12",
+        price : '10000',
+        img : '',
+        location : '여기'
+      }
+    ],
+    [
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매13",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매14",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매15",
+        price : '10000',
+        img : '',
+        location : '여기'
+      },
+      {
+        type:"공동구매",
+        title:"마라탕 공동구매16",
+        price : '10000',
+        img : '',
+        location : '여기'
+      }
+    ]
+  ]
 
   return (
     <div className="App">
@@ -22,8 +144,12 @@ function App() {
         {loginModalStatus ? <LoginModal setLoginModalStatus={setLoginModalStatus} onclickModalOnOff={onclickModalOnOff} />: ''}
         <Route path="/signup" component={SignUp}/>
         <Header loginStatus={loginStatus} onclickModalOnOff={onclickModalOnOff}/>
-        <Route exact path="/" component={MainPage} />
-        <Route path="mypage" component={MyPage}/>
+        <Route exact path="/">
+          <MainPage component={MainPage} RecomendGoddsArr={RecomendGoddsArr}/>
+          </Route>
+        <Route path="/mypage" >
+          <MyPage component={MyPage}/>
+          </Route>
         <Footer/>
       </BrowserRouter>
     </div>
--- src/Components/Pages/MyPage/MyPage.jsx
+++ src/Components/Pages/MyPage/MyPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './MyPage.scss';
 
-function MyPage(){
+function MyPage(props){
   return(
       <section className="myPage">
+          <div className="GoodsInfo">
+              <img src="" alt="" className="goodsImg" />
+              <section className="GoodsDetails">
+                  <h1 className="goodsTitle">마라탕 공동구매</h1>
+                  <div className="Price"><p className="goodsPrice">15000</p><p className="won">원</p></div>
+              </section>
+          </div>
       </section>
   );
 }
