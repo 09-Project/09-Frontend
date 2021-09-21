c2cf73ae92d49d4f6e61f92e0341954c0ebfba5c
paperdeer <tjrgus0713@gmail.com>
2021-09-22 01:04:17 +0900

    [추가] input에 글자수 제한

--- src/Components/Upload/Upload.jsx
+++ src/Components/Upload/Upload.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Upload.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
 function Upload(props){
     const {uploadType} = props;
+    const [textCount,setTextCount] = useState({
+        title : 0,
+        titleValue : '',
+        introduce : 0,
+        introduceValue : ''
+    });
+    const {title,introduce,titleValue,introduceValue} = textCount;
+    const limit = {
+        titleLimit : 20,
+        introduceLimit : 550
+    }
+    const {titleLimit,introduceLimit} = limit;
+    const onChangeTextCount = (e) => {
+        if((e.target.name === "title" && e.target.value.length <= titleLimit) || (e.target.name === "introduce" && e.target.value.length <= introduceLimit)){
+            setTextCount({
+                ...textCount,
+                [e.target.name] : e.target.value.length,
+                [e.target.name+'Value'] : e.target.value
+            })
+        }
+    }
   return(
       <section className="uploadPage">
           <div className="introducing"><h1>게시물 올리기</h1><p>*은 필수항목입니다</p></div>
@@ -17,11 +38,19 @@ function Upload(props){
             </div>
           </section>
           <section className="productInfo">
-              <div className="title"><h3>글 제목*</h3><input type="text" placeholder="글 제목을 입력해주세요" /><p className="letterCount">(0/20)</p></div>
-              <div className="introduce"><h3>글 설명</h3><textarea placeholder="글 설명을 입력해주세요"/><p>(0/550)</p></div>
+              <div className="title"><h3>글 제목*</h3><input name="title" type="text" placeholder="글 제목을 입력해주세요" onChange={onChangeTextCount} value={titleValue} /><p className="letterCount">({title}/{titleLimit})</p></div>
+              <div className="introduce"><h3>글 설명</h3><textarea name="introduce" placeholder="글 설명을 입력해주세요" onChange={onChangeTextCount} value={introduceValue}/><p>({introduce}/{introduceLimit})</p></div>
           </section>
+          {uploadType === "공동구매" ? 
           <section className="productPrice">
-              <div className="price">가격*</div><input type="text" placeholder="숫자만 입력해주세요" />원
+              <div className="price"><h3>가격*</h3><input type="text" placeholder="숫자만 입력해주세요" className="priceInput" /><p>원</p></div>
+          </section> : ''
+            }
+          <section className="productLocation">
+              <div className="location"><h3>거래지역*</h3><input type="text" placeholder="거래지역을 입력해주세요(ex.대덕소프트웨어마이스터고)" /></div>
+          </section>
+          <section className="productOpenChatting">
+              <div className="openChatting"><h3>오픈채팅*</h3><input type="text" placeholder="오픈채팅방 링크를 입력해주세요" /></div>
           </section>
       </section>
   );
