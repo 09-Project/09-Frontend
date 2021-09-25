ea90788bd3f801d48a1c161ca78e1b4c39d6bf09
paperdeer <tjrgus0713@gmail.com>
2021-09-25 21:37:55 +0900

    [업데이트] SignUpPage폴더 경로변경 및 props에 따라 input태그가 변하도록 변경

--- src/Components/SignUpPage/SignUp.jsx
+++ src/Components/SignUpPage/SignUp.jsx
@@ -5,7 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye,faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 
-function SignUp(){
+function SignUp(props){
+  const {changePassword} = props; // true면 마이페이지 false면 회원가입
   const postHeader = {
     'Content-type': 'application/json'
   }
@@ -26,24 +27,8 @@ function SignUp(){
   },[password])
   const onClickSubmit = useCallback(() => {
     if(makepassword === verifyPassword){
-      axios.post('http://3.19.60.23:8080/auth/join',{
-        name : nickName,
-        username : id,
-        password : makepassword
-      },{headers:postHeader})
-      .then(function(respose){
-        console.log(respose);
-        // setNickName('')
-        setPassword({
-          makepassword : '',
-          verifyPassword : ''
-        })
-      })
-      .error(function(){
-        alert("오류");
-      })
-    } else {
-      alert("오류");
+      if(changePassword) console.log("asd") // 마이페이지일때 post
+      else console.log("asd") // 회원가입일때 post
     }
   },[password,checkShow])
 
@@ -59,14 +44,15 @@ function SignUp(){
           <div className="logoInSignup">
             <i><img src="" alt="" /></i>
           </div>
-          <div className="joinUs">JOIN US</div>
+          {changePassword ? <h1 className="myPage signUpPageTitle">마이페이지</h1> :
+          <h1 className="joinUs signUpPageTitle">JOIN US</h1>}
           <div className="signUpInputContainer">
-            <p className="introduceInput">기본 정보 입력</p>
+            <p className="introduceInput">{changePassword ? "비밀번호 입력" : "기본 정보 입력"}</p>
             <div className="inputSection">
-              <div className="eachInput"><p>닉네임</p><input onChange={onChangePassword} name="nickName" type="text" value={nickName} /></div>
-              <div className="eachInput"><p>아이디</p><input name="makepassword" onChange={onChangePassword} name="id" type="text" value={id} /></div>
+              <div className="eachInput"><p>{changePassword ?"기존 비밀번호" : "닉네임"}</p><input onChange={onChangePassword} name="nickName" type="text" value={nickName} /></div>
+              {changePassword ? "" : <div className="eachInput"><p>아이디</p><input name="makepassword" onChange={onChangePassword} name="id" type="text" value={id} /></div>}
               <div className="eachInput">
-                <p>비밀번호</p>
+                <p>{changePassword ? "새 비밀번호" : "비밀번호"}</p>
                 <div>
                   <input name="makepassword" id="input" onChange={onChangePassword} type={checkShow[0] ? "text" : "password"} value={makepassword} />
                   <label onClick={()=>checkShowHide(0)}  htmlFor="input">
@@ -74,7 +60,7 @@ function SignUp(){
                 </div>
               </div>
               <div className="eachInput">
-                <p>비밀번호 확인</p>
+                <p>{changePassword ? "새 비밀번호 확인":"비밀번호 확인"}</p>
                 <div><input name="verifyPassword" id="check"  onChange={onChangePassword} type={checkShow[1] ? "text" : "password"} value={verifyPassword} />
                   <label  onClick={()=>checkShowHide(1)} htmlFor="check">
                     <FontAwesomeIcon  icon={checkShow[1] ? faEye : faEyeSlash}/>
