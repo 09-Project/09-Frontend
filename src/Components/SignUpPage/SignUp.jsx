fd59d979fb4d36bc8cf5c3fdb6646b70fcc533c8
paperdeer <tjrgus0713@gmail.com>
2021-09-28 14:44:36 +0900

    [업데이트] useQuery를 사용하여 회원가입, 비밀번호 변경시 각각 다른 화면을 보여주도록 변경

--- src/Components/SignUpPage/SignUp.jsx
+++ src/Components/SignUpPage/SignUp.jsx
@@ -3,10 +3,13 @@ import './SignUp.scss';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye,faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
-
+import { Link,useLocation } from 'react-router-dom';
 function SignUp(props){
-  const {changePassword} = props; // true면 마이페이지 false면 회원가입
+  function useQuery(){
+    return new URLSearchParams(useLocation().search);
+  }
+  const query = useQuery();
+  const isRectify =  query.get("name");
   const postHeader = {
     'Content-type': 'application/json'
   }
@@ -27,8 +30,8 @@ function SignUp(props){
   },[password])
   const onClickSubmit = useCallback(() => {
     if(makepassword === verifyPassword){
-      if(changePassword) console.log("asd") // 마이페이지일때 post
-      else console.log("asd") // 회원가입일때 post
+      if(isRectify) console.log("Asd") // 비번 변경
+      else console.log('asd') // 회원가입
     }
   },[password,checkShow])
 
@@ -44,16 +47,16 @@ function SignUp(props){
           <div className="logoInSignup">
             <i><img src="" alt="" /></i>
           </div>
-          {changePassword ? <h1 className="myPage signUpPageTitle">마이페이지</h1> :
+          {isRectify ? <h1 className="myPage signUpPageTitle">마이페이지</h1> :
           <h1 className="joinUs signUpPageTitle">JOIN US</h1>}
             <div className="signUpInputContainer">
-              <p className="introduceInput">{changePassword ? "비밀번호 입력" : "기본 정보 입력"}</p>
+              <p className="introduceInput">{isRectify ? "비밀번호 입력" : "기본 정보 입력"}</p>
               <div className="SignUpBody">
               <div className="inputSection">
-                <div className="eachInput"><p>{changePassword ?"기존 비밀번호" : "닉네임"}</p><input onChange={onChangePassword} name="nickName" type="text" value={nickName} /></div>
-                {changePassword ? "" :<div className="eachInput"><p>아이디</p><input name="makepassword" onChange={onChangePassword} name="id" type="text" value={id} /></div> }
+                <div className="eachInput"><p>{isRectify ?"기존 비밀번호" : "닉네임"}</p><input onChange={onChangePassword} name="nickName" type="text" value={nickName} /></div>
+                {isRectify ? "" :<div className="eachInput"><p>아이디</p><input name="makepassword" onChange={onChangePassword} name="id" type="text" value={id} /></div> }
                 <div className="eachInput">
-                  <p>{changePassword ? "새 비밀번호" : "비밀번호"}</p>
+                  <p>{isRectify ? "새 비밀번호" : "비밀번호"}</p>
                   <div>
                     <input name="makepassword" id="input" onChange={onChangePassword} type={checkShow[0] ? "text" : "password"} value={makepassword} />
                     <label onClick={()=>checkShowHide(0)}  htmlFor="input">
@@ -61,7 +64,7 @@ function SignUp(props){
                   </div>
                 </div>
                 <div className="eachInput">
-                  <p>{changePassword ? "새 비밀번호 확인":"비밀번호 확인"}</p>
+                  <p>{isRectify ? "새 비밀번호 확인":"비밀번호 확인"}</p>
                   <div><input name="verifyPassword" id="check"  onChange={onChangePassword} type={checkShow[1] ? "text" : "password"} value={verifyPassword} />
                     <label  onClick={()=>checkShowHide(1)} htmlFor="check">
                       <FontAwesomeIcon  icon={checkShow[1] ? faEye : faEyeSlash}/>
