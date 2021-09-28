ef09c3357b3244f1146dc770a276a9331b2ebdff
paperdeer <tjrgus0713@gmail.com>
2021-09-28 10:12:09 +0900

    [추가] 메인페이지에 추천 상품리스트를 바꾸는 버튼 추가

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.scss';
 import GoodsBox from '../../Components/GoodsBox/GoodsBox';
 
 function MainPage(props){
   const {RecomendGoddsArr,myWishList} = props;
+  const [selected,setSelected] = useState([true,false,false,false,false]);
+  const onClickSelectBtn = (e) => {
+    const nextArr = [false,false,false,false,false];
+    nextArr[e] = true;
+    setSelected(nextArr);
+  }
   return(
       <div className="MainPage">
           <div className="AD"></div>
@@ -17,6 +23,17 @@ function MainPage(props){
               )}
             </section>
           </div>
+          <section className="changeRecommendedPageSection">
+                <ul className="changeBtns">
+                  <li className="eachBtn arrow">{"<"}</li>
+                  <li onClick={()=>onClickSelectBtn(0)} className={ selected[0] ? " selected eachBtn" : "eachBtn isNotSelected"}>1</li>
+                  <li onClick={()=>onClickSelectBtn(1)} className={ selected[1] ? " selected eachBtn" : "eachBtn isNotSelected"}>2</li>
+                  <li onClick={()=>onClickSelectBtn(2)} className={ selected[2] ? " selected eachBtn" : "eachBtn isNotSelected"}>3</li>
+                  <li onClick={()=>onClickSelectBtn(3)} className={ selected[3] ? " selected eachBtn" : "eachBtn isNotSelected"}>4</li>
+                  <li onClick={()=>onClickSelectBtn(4)} className={ selected[4] ? " selected eachBtn" : "eachBtn isNotSelected"}>5</li>
+                  <li className="eachBtn arrow">{">"}</li>
+                </ul>
+          </section>
       </div>
   );
 }
