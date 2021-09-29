bee131650d442bb985e8705c32c6b3d10637bba7
paperdeer <tjrgus0713@gmail.com>
2021-09-29 14:41:47 +0900

    [추가] product 컴퍼넌트에 상품 설명과 판매자 정보 추가

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -25,6 +25,38 @@ function Product(props){
                     <li className="removeProduct"><button>삭제</button></li>
               </ul>
           </div>
+              <section className="productInformations">
+                  <ul className="introduceType"><li><p className="productIntroduce">상품설명</p></li><li><p className="wirterInfo">판매자 정보</p></li></ul>
+                  <div className="contents">
+                      <p className="introduceContent">
+                        제가 제 뒷자리에 있는 혜령이한테 지금 제일 먹고싶은게 뭐냐고 물어보니까 마라탕이 제일 먹고싶다고 하더
+                        라고요 그래서 디자인을 마라탕 공동구매와 마라탕 기부로 채워봤어요!!! 그런데 저는 마라탕보다 지금 그 뭐
+                        지 아 허니콤보랑 엽떡이 먹고 싶더라고요 그래서 집가서 먹으려고요!! 제가 제 뒷자리에 있는 혜령이한테 지
+                        금 제일 먹고싶은게 뭐냐고 물어보니까 마라탕이 제일 먹고싶다고 하더라고요 그래서 디자인을 마라탕 공동구
+                        매와 마라탕 기부로 채워봤어요!!! 그런데 저는 마라탕보다 지금 그 뭐지 아 허니콤보랑 엽떡이 먹고 싶더라고
+                        요 그래서 집가서 먹으려고요!! 제가 제 뒷자리에 있는 혜령이한테 지금 제일 먹고싶은게 뭐냐고 물어보니까 
+                        마라탕이 제일 먹고싶다고 하더라고요 그래서 디자인을 마라탕 공동구매와 마라탕 기부로 채워봤어요!!! 그런
+                        데 저는 마라탕보다 지금 그 뭐지 아 허니콤보랑 엽떡이 먹고 싶더라고요 그래서 집가서 먹으려고요!! 이거까지 
+                        쓰면 대충 500 글자 채움 이거까지 쓰면 500 글자
+                    </p>
+                <section className="wirterInfoContents">
+                    <div className="profile">
+                        <div className="profileTop">
+                            <img src="" alt="" />
+                            <div className="details">
+                                <p className="Writername">추혜연</p>
+                                <div><p className="productCount">상품 2</p><p className="Wish">찜 13</p></div>
+                                <button>페이지 방문</button>
+                            </div>
+                        </div>
+                        <p className="shortIntroduce">
+                            아 배고프다 오늘 급식 뭐였지 아 배고프다 오늘 급
+                            식 뭐였지 아 배고프다 오늘 급식 뭐였지
+                        </p>
+                    </div>
+                </section>
+                </div>
+            </section>
       </section>
   );
 }
