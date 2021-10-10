7a97df9ca3845050c7343c3eb5f129efc7da0c9e
강석현 <tjrgus0713@gmail.com>
2021-10-11 03:08:32 +0900

    [추가] 제품 상세페이지에서 다른 상품 추천 부분의 get API를 받아와서 보여주도록 추가 및 다른 상품 추천부분의 박스를 클릭시 해당 상품의 상세페이지로 이동하도록 추가

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,61 +1,40 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './Product.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart,faLink,faUser } from '@fortawesome/free-solid-svg-icons'
-
+import axios from 'axios';
+import { useHistory, useLocation } from 'react-router';
+import { API_HOST } from '../../constant/api';
+import { Link } from 'react-router-dom';
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 function Product(props){
-    const objInfo = {
-        title : '마라탕 공동구매',
-        objPrice : 15000,
-        hearts : 4,
-        link : 'https://open.kakao.com/o/sjPi6pR',
-        writer : '추혜연'
-    }
-    const arr = [
-        {
-            name : 'asd',
-            img : ''
-        },
-        {
-            name : 'sd',
-            img : ''
-        },
-        {
-            name : 'ddd',
-            img : ''
-        },
-        {
-            name : 'qwe',
-            img : ''
-        },
-        {
-            name : 'z',
-            img : ''
-        },
-        {
-            name : 'a',
-            img : ''
-        },
-        {
-            name : 's',
-            img : ''
-        },
-        {
-            name : 'd',
-            img : ''
-        },
-    ]
-  return(
-      <section className="Product">
+    const [otherProduct,setOhterProduct] = useState([]);
+    const location = useLocation()
+    useEffect(()=>{
+        axios.get(API_HOST+'/post/other').then(res=>setOhterProduct(res.data)); 
+    },[])
+    useEffect(()=>{
+        console.log(otherProduct);
+    },[otherProduct])
+    const [productContent,setProductContent] = useState('');
+    let query = useQuery();
+    const post_id = query.get('post_id')
+    useEffect(()=>{
+        axios.get(API_HOST+'/post/'+post_id).then(res=>setProductContent(res.data));
+    },[location.search]);
+    return(
+        <section className="Product">
           <div className="GoodsInfo">
-              <img src="" alt="" className="goodsImg" />
+              <img src={productContent.image} alt="" className="goodsImg" />
               <ul className="GoodsDetails">
-                    <li className="goodsTitle">{objInfo.title}</li>
-                    <li className="Price"><p className="goodsPrice"></p>{objInfo.objPrice}<p className="won">원</p></li>
+                    <li className="goodsTitle">{productContent.title}</li>
+                    <li className="Price"><p className="goodsPrice"></p>{productContent.price}<p className="won">원</p></li>
                     <li className="line"></li>
-                    <li className="heartCount"><FontAwesomeIcon icon={faHeart} className="iconInDetail"/><p>{objInfo.hearts}</p></li>
-                    <li className="chattingLink"><FontAwesomeIcon icon={faLink}  className="iconInDetail"/><a href={objInfo.link}>{objInfo.link}</a></li>
-                    <li className="wirter"><FontAwesomeIcon icon={faUser}className="iconInDetail"/><p>{objInfo.writer}</p></li>
+                    <li className="heartCount"><FontAwesomeIcon icon={faHeart} className="iconInDetail"/><p>{productContent.get_likes}</p></li>
+                    <li className="chattingLink"><FontAwesomeIcon icon={faLink}  className="iconInDetail"/><a href={productContent.open_chat_link}>{productContent.open_chat_link}</a></li>
+                    <li className="wirter"><FontAwesomeIcon icon={faUser}className="iconInDetail"/><p>{productContent.member_name}</p></li>
                     <li className="removeProduct"><button>삭제</button></li>
               </ul>
           </div>
@@ -63,29 +42,20 @@ function Product(props){
                   <ul className="introduceType"><li><p className="productIntroduce">상품설명</p></li><li><p className="wirterInfo">판매자 정보</p></li></ul>
                   <div className="contents">
                       <p className="introduceContent">
-                        제가 제 뒷자리에 있는 혜령이한테 지금 제일 먹고싶은게 뭐냐고 물어보니까 마라탕이 제일 먹고싶다고 하더
-                        라고요 그래서 디자인을 마라탕 공동구매와 마라탕 기부로 채워봤어요!!! 그런데 저는 마라탕보다 지금 그 뭐
-                        지 아 허니콤보랑 엽떡이 먹고 싶더라고요 그래서 집가서 먹으려고요!! 제가 제 뒷자리에 있는 혜령이한테 지
-                        금 제일 먹고싶은게 뭐냐고 물어보니까 마라탕이 제일 먹고싶다고 하더라고요 그래서 디자인을 마라탕 공동구
-                        매와 마라탕 기부로 채워봤어요!!! 그런데 저는 마라탕보다 지금 그 뭐지 아 허니콤보랑 엽떡이 먹고 싶더라고
-                        요 그래서 집가서 먹으려고요!! 제가 제 뒷자리에 있는 혜령이한테 지금 제일 먹고싶은게 뭐냐고 물어보니까 
-                        마라탕이 제일 먹고싶다고 하더라고요 그래서 디자인을 마라탕 공동구매와 마라탕 기부로 채워봤어요!!! 그런
-                        데 저는 마라탕보다 지금 그 뭐지 아 허니콤보랑 엽떡이 먹고 싶더라고요 그래서 집가서 먹으려고요!! 이거까지 
-                        쓰면 대충 500 글자 채움 이거까지 쓰면 500 글자
+                        {productContent.content}
                     </p>
                 <section className="wirterInfoContents">
                     <div className="profile">
                         <div className="profileTop">
                             <img src="" alt="" />
                             <div className="details">
-                                <p className="Writername">추혜연</p>
-                                <div><p className="productCount">상품 2</p><p className="Wish">찜 13</p></div>
+                                <p className="Writername">{productContent.member_name}</p>
+                                <div><p className="productCount">상품 {productContent.posts_count}</p><p className="Wish">찜 {productContent.every_like_counts}</p></div>
                                 <button>페이지 방문</button>
                             </div>
                         </div>
                         <p className="shortIntroduce">
-                            아 배고프다 오늘 급식 뭐였지 아 배고프다 오늘 급
-                            식 뭐였지 아 배고프다 오늘 급식 뭐였지
+                            {productContent.member_introduction}
                         </p>
                     </div>
                 </section>
@@ -94,11 +64,11 @@ function Product(props){
             <section className="RecommendOtherProduct">
                 <p className="showOtherProduct">다른 상품 보기</p>
                 <div className="ProductLists">
-                    {arr.map((li)=>
-                        <div className="eachList">
-                            <img src={li.img} alt="" />
-                            <p>{li.name}</p>
-                        </div>
+                    {otherProduct.map((li)=>
+                        <Link to={`/product?post_id=${li.id}`} className="eachList">
+                            <img src={li.image} alt="" />
+                            <p>{li.title}</p>
+                        </Link>
                     )}
                 </div>
             </section>
