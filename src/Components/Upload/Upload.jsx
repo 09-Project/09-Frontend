55058d72ccdcb229cd434dfe4a1cb3f0347479f5
강석현 <tjrgus0713@gmail.com>
2021-10-19 17:12:21 +0900

    [수정] postContent useState재정비

--- src/Components/Upload/Upload.jsx
+++ src/Components/Upload/Upload.jsx
@@ -10,57 +10,27 @@ const DONATION_RADIO_ID = 'donation';
 
 function Upload(props){
     const [selectedRadio,setSelectedRadio] = useState("co-buying");
-    const [textCount,setTextCount] = useState({
-        title : 0,
-        titleValue : '',
-        introduce : 0,
-        introduceValue : ''
-    });
-    const {title,introduce,titleValue,introduceValue} = textCount;
-    const limit = {
-        titleLimit : 20,
-        introduceLimit : 550
-    }
-    const {titleLimit,introduceLimit} = limit;
     const onChangeRadio = (event) => {
         const { id } = event.target;
-
         setSelectedRadio(id);
     }
-    const onChangeTextCount = (e) => {
-        if((e.target.name === "title" && e.target.value.length <= titleLimit) || (e.target.name === "introduce" && e.target.value.length <= introduceLimit)){
-            setTextCount({
-                ...textCount,
-                [e.target.name] : e.target.value.length,
-                [e.target.name+'Value'] : e.target.value
-            })
-        }
-    }
-    const [textContent,setTextContent] = useState({
-        price : 0,
-        location : '',
-        openChatting : ''
+    const [postContent,setPostContent] = useState({
+        title : '',
+        content : '',
+        price : '',
+        transactionRegion : '',
+        openChatLink : '',
+        image : ''
     })
-    const {price,location,openChatting} = textContent;
-    const onChangeTextContent = (e) => {
-        const nextObj = {
-            ...textContent,
+    const {title,content,price,transactionRegion,openChatLink,image} = postContent;
+    const onClickPostContent = e => {
+        setPostContent({
+            ...postContent,
             [e.target.name] : e.target.value
-        }
-        setTextContent(nextObj);
+        })
     }
-    const onClickSubmit = () => {
-        axios.post(API_HOST+'/post',{
-            "title" : title,
-            "content" : introduce,
-            "price" : price,
-            "transactionRegion" : location,
-            "openChatLink" : openChatting,
-            "image" : 'ㅁㄴㅇ'
-        })
-        .then(function(response){
-            console.log(response);
-        })
+    const onSubmitPost = () => {
+        axios.post(API_HOST + '/post',postContent).then(res=>{})
     }
   return(
       <section className="uploadPage">
@@ -73,27 +43,27 @@ function Upload(props){
             <div className="imgCount"><p>상품 이미지</p><p>(3/4)</p></div>
             <div className="uploadImgs">
                 <ul className="imgs">
-                    <li className="eachImg"><FontAwesomeIcon className="img" icon={faCamera}/><p>이미지 등록</p></li>
+                    <label className="eachImg"><FontAwesomeIcon className="img" icon={faCamera}/><p>이미지 등록</p></label>
                 </ul>
                 <p>- 이미지는 상품등록 시 정사각형으로 잘려서 등록됩니다.</p>
             </div>
           </section>
           <section className="productInfo">
-              <div className="title"><h3>글 제목*</h3><input name="title" type="text" placeholder="글 제목을 입력해주세요" onChange={onChangeTextCount} value={titleValue} /><p className="letterCount">({title}/{titleLimit})</p></div>
-              <div className="introduce"><h3>글 설명</h3><textarea name="introduce" placeholder="글 설명을 입력해주세요" onChange={onChangeTextCount} value={introduceValue}/><p>({introduce}/{introduceLimit})</p></div>
+              <div className="title"><h3>글 제목*</h3><input name="title" type="text" placeholder="글 제목을 입력해주세요" onChange={onClickPostContent} value={title} /><p className="letterCount">({}/{})</p></div>
+              <div className="introduce"><h3>글 설명</h3><textarea name="content" placeholder="글 설명을 입력해주세요" onChange={onClickPostContent} value={content}/><p>({}/{})</p></div>
           </section>
           {selectedRadio === CO_BUYING_RADIO_ID ? 
           <section className="productPrice">
-              <div className="price"><h3>가격*</h3><input name="price" onChange={onChangeTextContent} type="text" placeholder="숫자만 입력해주세요" className="priceInput" /><p>원</p></div>
+              <div className="price"><h3>가격*</h3><input name="price" onChange={onClickPostContent} type="text" placeholder="숫자만 입력해주세요" value={price} className="priceInput" /><p>원</p></div>
           </section> : ''
             }
           <section className="productLocation">
-              <div className="location"><h3>거래지역*</h3><input name="location" onChange={onChangeTextContent} type="text" placeholder="거래지역을 입력해주세요(ex.대덕소프트웨어마이스터고)" /></div>
+              <div className="location"><h3>거래지역*</h3><input name="transactionRegion" onChange={onClickPostContent} type="text" placeholder="거래지역을 입력해주세요(ex.대덕소프트웨어마이스터고)" value={transactionRegion} /></div>
           </section>
           <section className="productOpenChatting">
-              <div className="openChatting"><h3>오픈채팅*</h3><input name="openChatting" onChange={onChangeTextContent} type="text" placeholder="오픈채팅방 링크를 입력해주세요" /></div>
+              <div className="openChatting"><h3>오픈채팅*</h3><input name="openChatLink" onChange={onClickPostContent} type="text" value={openChatLink} placeholder="오픈채팅방 링크를 입력해주세요" /></div>
           </section>
-          <div className="completion"><button onClick={onClickSubmit}>완료</button></div>
+          <div className="completion"><button onClick={onSubmitPost}>완료</button></div>
       </section>
   );
 }
