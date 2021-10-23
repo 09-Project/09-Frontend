1701bd146c12e7639a9ca6846d59a1fe430c19ac
강석현 <tjrgus0713@gmail.com>
2021-10-23 23:26:48 +0900

    [수정] 필요없는 코드 삭제

--- src/App.js
+++ src/App.js
@@ -44,55 +44,6 @@ function App() {
       price : '10000',
       img : '',
       location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매2",
-      price : '10000',
-      img : '',
-      location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매3",
-      price : '10000',
-      img : '',
-      location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매4",
-      price : '10000',
-      img : '',
-      location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매5",
-      price : '10000',
-      img : '',
-      location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매6",
-      price : '10000',
-      img : '',
-      location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매7",
-      price : '10000',
-      img : '',
-      location : '여기'
-    },
-    {
-      type:"공동구매",
-      title:"마라탕 공동구매8",
-      price : '10000',
-      img : '',
-      location : '여기'
     }
   ])
   const [myWishList,setMyWishList] = useState([
@@ -100,47 +51,7 @@ function App() {
       img : '',
       title : '마라탕 공동구매1',
       status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매2',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매3',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매4',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매5',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매6',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매7',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매8',
-      status : false
-    },
-    {
-      img : '',
-      title : '마라탕 공동구매9',
-      status : false
-    },
+    }
   ])
   const [selectedTypeBox,setSelectedTypeBox] = useState([true,false,false]);
   return (
